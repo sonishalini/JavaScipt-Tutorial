0be421b71a9cf58c27494d08bfed9017415b9ba8
sonishalini <shalinisoni2510@gmail.com>
2021-04-26 09:31:30 -0700

    JavaScript & JavaScript Console API

---
+++ index.js
@@ -0,0 +1,13 @@
+//1.""Ways to print in JavaScript"""
+console.log("hello world");
+//alert("me"); this tag is not used!
+// document.write("This is document write");
+
+//""2. JavaScript console API"""
+console.log("hello world", 4 + 6, "Another log");
+//alert("me"); this tag is not used!
+// document.write("This is document write");
+console.warn("this a warning");
+//Error identify karne ke liye use karte hai
+//kahi bhi kisi bhi jagah par Error ko lock karna hua to use karege
+console.error("This is an error");
