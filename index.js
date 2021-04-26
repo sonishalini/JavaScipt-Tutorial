c2de25648779fd01e05bd152dd0e14d735a03849
sonishalini <shalinisoni2510@gmail.com>
2021-04-26 10:16:17 -0700

    JavaScript Variables are completed

--- index.js
+++ index.js
@@ -12,10 +12,48 @@ console.warn("this a warning");
 //kahi bhi kisi bhi jagah par Error ko lock karna hua to use karege
 console.error("This is an error");
 
-//JavaScript Variables
+//3. JavaScript Variables
 //What are Variables? ----Containers to store data values
 //Single Line Comment
 /* Multi Line Comment */
 var number1 = 45;
 var number2 = 55;
 console.log(number1 + number2);
+
+// 4. Data Type  in JavaScript
+// Number
+num1 = 458;
+num2 = 556.5;
+
+// String
+var str1 = "This is a string.";
+var str2 = "This is also a string.";
+
+//object
+var marks = {
+  ravi: 34,
+  sbhubham: 43,
+  tommy: 54,
+};
+console.log(marks);
+
+//Booleans
+var a = true;
+var b = false;
+console.log(a, b);
+
+//var und= undefined
+var und;
+console.log(und);
+// Null
+var n = null;
+console.log(n);
+
+/*
+At a very high level, ther are 2 types of data type in JavaScript
+1.Primitive Data Types: Undefined,Null,Number,String,Boolean,Symbol
+2.Reference Data Types: Array and Objects
+*/
+
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
