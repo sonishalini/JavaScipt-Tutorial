bd89a3b2f40ccbdece75784367ee4cad5d56ba2a
sonishalini <shalinisoni2510@gmail.com>
2021-04-26 09:42:17 -0700

    Variables Pass

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ console.warn("this a warning");
 //Error identify karne ke liye use karte hai
 //kahi bhi kisi bhi jagah par Error ko lock karna hua to use karege
 console.error("This is an error");
+
+//JavaScript Variables
+//What are Variables? ----Containers to store data values
+//Single Line Comment
+/* Multi Line Comment */
+var number1 = 45;
+var number2 = 55;
+console.log(number1 + number2);
